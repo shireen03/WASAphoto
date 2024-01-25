50d826ced46d96061f5360a3190f8342c6903702
shireen03 <shireen.aynetchi@gmail.com>
2024-01-25 13:52:11 +0100

    some cleanup

--- open-npm.sh
+++ open-npm.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-docker run -it --rm -v "$(pwd):/src" -u "$(id -u):$(id -g)" --network host --workdir /src/webui -e NPM_CONFIG_CACHE=webui/.npm node:lts /bin/bash
+docker run -it --rm -v "$(pwd):/src" -u "$(id -u):$(id -g)" --network host --workdir /src/webui node:lts /bin/bash
